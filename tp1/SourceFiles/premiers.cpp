471c1d5e49e42909f990ab978f6e3f77880588c2
Houssem Sebouai <sboui.houssem@gmail.com>
2017-02-13 19:35:24 -0500

    Cleaning and optimizing

--- tp1/SourceFiles/premiers.cpp
+++ tp1/SourceFiles/premiers.cpp
@@ -12,16 +12,17 @@
 #include <math.h>
 
 
-#define THREADS_NUMBER  4
+#define THREADS_NUMBER  5
 // Global shared varianles
 char *gFlags;
-unsigned long gMax;
-int gCount = 0;
-long gCandidate;
+unsigned long gMax, gCandidate;
+int gCount, gAlgoType =0;
 pthread_t gIds[THREADS_NUMBER];
 
 // Initialization Mutex
 pthread_mutex_t gCandidateLock = PTHREAD_MUTEX_INITIALIZER;
+
+
 
 // Method declaration
 void *invalidate_pairs_number(void *iArgs);
@@ -35,46 +36,55 @@ int main(int argc, char *argv[]) {
     // If no argument given then,
     // the minimal default search limit is 1000
     gMax = 1000;
-    //
-    if (argc >= 2) {
-        gMax = atol(argv[1]);
+    switch (argc) {
+        case 2:
+            gMax = (unsigned long) atol(argv[1]);
+            printf("No type of algorithm given, the program will run in sequential mode.\n");
+            break;
+        case 3:
+            gMax = (unsigned long) atol(argv[1]);
+            gAlgoType = atoi(argv[2]);
+            break;
+        default:
+            printf("No arguments Given:"
+                           "Default limit is %ld;\n"
+                           "Default mode is sequential.\n", gMax);
     }
-
 
     // Start chronometer
     Chrono lChrono(true);
 
     // Allocate the prime flag table in the our memory
     gFlags = (char *) calloc(gMax, sizeof(*gFlags));
-    printf("l'adresse de l'allocation du tableau de flag est %ld \n", gFlags);
     assert(gFlags != 0);
-    int j = 0;
+//    printf("l'adresse de l'allocation du tableau de flag est %c\n", gFlags);
 
-    
 
     gCandidate = 3;
 
     // create threads
     //printf("Starting threads\n");
-    if (THREADS_NUMBER >1) {
-        for (int i = 1; i <= THREADS_NUMBER; ++i) {
-           // printf("Starting thread number %d \n", i);
+    if (gAlgoType == 1) {
+        for (int i = 1; i <= THREADS_NUMBER; i++) {
+            // printf("Starting thread number %d \n", i);
+            if (i == 1) {
                 // Invalidate pair numbers as primes
-            if (i ==1) pthread_create(&gIds[0], NULL, invalidate_pairs_number, (void*) j);
-                // Search for other primes
-            else pthread_create(&gIds[i - 1], NULL, find_multiple, (void *) i);
+                pthread_create(&gIds[0], NULL, invalidate_pairs_number, NULL);
+            } else {
+                // Search for other primes in non-pairs numbers
+                pthread_create(&gIds[i - 1], NULL, find_multiple, NULL);
+            }
+
         }
-    
+
         // wait for thread completion
         //printf("Joining threads\n");
-        for(int i=1; i<=THREADS_NUMBER; ++i) {
-            void **lWorkDone = NULL;
-            pthread_join(gIds[i - 1], lWorkDone);
-           // printf("Joining thread number %d \n", i);
+        for (int i = 1; i <= THREADS_NUMBER; i++) {
+            pthread_join(gIds[i - 1], NULL);
+//            printf("Joining thread number %d \n", i);
         }
-    }
-    else {
-    // Serial basic Sieve of Eratosthenes algorithm
+    } else {
+        // Serial basic Sieve of Eratosthenes algorithm
         for (unsigned long p = 2; p < gMax; p++) {
             if (gFlags[p] == 0) {
                 // Invalidate all multiple of p
@@ -83,17 +93,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
-
-
     // Stop Chronometer
     lChrono.pause();
-
 
     // Count primes numbers and output them on the console
     for (unsigned long p = 2; p < gMax; p++) {
         if (gFlags[p] == 0) {
-            if (gCount < 20)printf("%ld ", p);
+            if (gCount < 100)printf("%ld ", p);
             gCount += 1;
         }
     }
@@ -106,12 +112,11 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void *invalidate_pairs_number(void *iArgs) {
-    unsigned long  lPrimeWorkDone = 0;
-    for (long i = 3; i < gMax; i++) {
-        if (i % 2 == 0) gFlags[i]++;
+void *invalidate_pairs_number(void *) {
+    for (unsigned long i = 4; i < gMax; i += 2) {
+        gFlags[i]++;
     }
-    pthread_exit((void *) lPrimeWorkDone);
+    pthread_exit(NULL);
 }
 
 void invalidate_multiple(unsigned long p) {
@@ -120,21 +125,20 @@ void invalidate_multiple(unsigned long p) {
     }
 }
 
-void *find_multiple(void *iArg) {
-    unsigned long lLimit, lCandidate, lPrimeWorkDone = 0;
+void *find_multiple(void *) {
+    unsigned long lLimit, lCandidate = 0;
     do {
-        lLimit = sqrt(gMax);
+        lLimit = (unsigned long) sqrt(gMax);
         pthread_mutex_lock(&gCandidateLock);
         lCandidate = gCandidate;
         gCandidate += 2;
         pthread_mutex_unlock(&gCandidateLock);
         if (lCandidate <= lLimit) {
-            if(!gFlags[lCandidate]){
+            if (!gFlags[lCandidate]) {
                 invalidate_multiple(lCandidate);
-                //lPrimeWorkDone++;
             }
-        }else{
-            pthread_exit((void *) lPrimeWorkDone);
+        } else {
+            pthread_exit(NULL);
         }
-    }while (1);
+    } while (1);
 }
