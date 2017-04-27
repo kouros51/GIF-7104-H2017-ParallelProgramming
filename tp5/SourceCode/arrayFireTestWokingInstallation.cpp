a77595d390a553d5d96c8e4d271a4db474b91a4a
kouros51 <sboui.houssem@gmail.com>
2017-04-27 02:04:29 -0400

    Different backend OpenCL failing in my computer

--- tp5/SourceCode/arrayFireTestWokingInstallation.cpp
+++ tp5/SourceCode/arrayFireTestWokingInstallation.cpp
@@ -9,28 +9,50 @@
 #include <arrayfire.h>
 
 
+void testMatrix();
+
 using namespace af;
 using namespace std;
 
-int main (int argc,char **argv){
+int main(int argc, char **argv) {
+
+//    int device = argc > 1 ? atoi(argv[1]) : 0;
+//    setDevice(device);
+
     try {
+        cout << "Trying CPU Backend" << endl;
+        setBackend(AF_BACKEND_CPU);
         unsigned int time = clock();
-        int device = argc > 1 ? atoi(argv[1]) : 0 ;
-        setDevice(device);
-        info();
-        printf("Create a 5-by-3 matrix of random floats on GPU ?????\n");
-        array A = randu(5,5, f32);
-        af_print(A);
+        testMatrix();
 
-        time = clock() - time ;
-        printf("Executed in %f seconds.\n", ((float)time/(float)CLOCKS_PER_SEC));
+        time = clock() - time;
+        printf("Executed in %f seconds.\n", ((float) time / (float) CLOCKS_PER_SEC));
 
-    } catch (af::exception& e) {
-        fprintf(stderr, "%s\n",e.what());
-        throw ;
+    } catch (af::exception &e) {
+        cout << "Failed CPU Backend" << endl;
+        fprintf(stderr, "%s\n", e.what());
+        throw;
+    }
+
+    try {
+        cout << "Trying OpenCL Backend" << endl;
+        setBackend(AF_BACKEND_CUDA);
+        testMatrix();
+
+    } catch (af::exception &e) {
+        cout << "Failed OpenCL Backend" << endl;
+        fprintf(stderr, "%s\n", e.what());
+        throw;
     }
 
     printf("\n\n=========================\n");
-    cout<< "Yay This is working"<<endl;
+    cout << "Yay This is working" << endl;
     return 0;
+}
+
+void testMatrix() {
+    info();
+    printf("Create a 5-by-3 matrix of random floats on GPU ?????\n");
+    array A = randu(5, 5, f32);
+    af_print(A);
 }
