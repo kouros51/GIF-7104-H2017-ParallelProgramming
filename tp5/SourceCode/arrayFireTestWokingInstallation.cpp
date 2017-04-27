dbefd1af1a828ca6441db0405ffd561219655f13
kouros51 <sboui.houssem@gmail.com>
2017-04-26 23:14:46 -0400

    Initial organisation

---
+++ tp5/SourceCode/arrayFireTestWokingInstallation.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kouros51 on 4/26/17.
+// To test if the Array fire installation is working
+// We use a simple Hello Word program
+//
+
+
+
+using namespace af;
+
+int main (int argc,char **argv){
+    return 0;
+}
